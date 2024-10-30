526458ec33f888c59012137d2d6e604d9d76b116
Harshit Gupta <44468199+harshit432@users.noreply.github.com>
2024-10-30 22:06:16 +0530

    Create euler.py

---
+++ euler.py
@@ -0,0 +1,8 @@
+# import sympy 
+from sympy import * n = 4
+print("Value of n = {}".format(n)) 
+
+# Use sympy.euler() method 
+nth_euler = euler(n) 
+	
+print("Value of nth euler number : {}".format(nth_euler)) 
